e865bbcbde3a52df6a9cf3e4518c95921abbaa2a
Daniel Villavicencio <daniel124@usf.edu>
2021-09-21 23:03:36 -0400

    Tech Project 3

--- Tech_Project_3/Program.cs
+++ Tech_Project_3/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+    Daniel Villavicencio
+    09/21/2021
+    Tech Project 3
+    Iterative Statements
+*/
+
+using System;
 
 namespace Tech_Project_3
 {
@@ -10,6 +13,38 @@ namespace Tech_Project_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kilometer increment value = ");
+
+            try
+            {
+                string input = Console.ReadLine();
+                int km = int.Parse(input);
+
+                Console.WriteLine("Kilometer Increment Value = " + km);
+                Console.WriteLine("{0} {1}", "Kilometers ", "Equivalent Miles");
+                Console.WriteLine("{0} {1}", "0 ", "         0");
+
+                do
+                {
+                    int INCREMENT = km;
+                    do
+                    {
+                        double miles = km * 0.621371;
+                        
+                        Console.WriteLine(Convert.ToString(km) + "         " + miles);
+
+
+                        km += INCREMENT;
+                    }
+                    while (km <= 200);
+                }
+                while (km >= 10 && km <=25);
+
+            }
+            catch
+            {
+
+            }
         }
     }
 }
